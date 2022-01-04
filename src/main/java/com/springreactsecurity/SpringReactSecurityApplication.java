10143a93a9b6ecd5a8e01d8494eda1911f65836a
Junhan0037 <wnsgks0037@naver.com>
2022-01-04 13:54:06 +0900

    Initial commit

---
+++ src/main/java/com/springreactsecurity/SpringReactSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springreactsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springreactsecurity/SpringReactSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreactsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
