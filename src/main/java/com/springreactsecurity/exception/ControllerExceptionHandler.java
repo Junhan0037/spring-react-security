82db9a26ef6e216869637024e5df6188613cb532
Junhan0037 <wnsgks0037@naver.com>
2022-01-04 15:17:13 +0900

    feat: 에러 핸들링 #1

---
+++ src/main/java/com/springreactsecurity/exception/ControllerExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.springreactsecurity.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+
+}
